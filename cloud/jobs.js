a17eb76a3cbc5670e1abb10ff4e23f93f570a130
Byland Zhang <Byland Zhang>
2016-01-22 22:45:42 +0800

    jobs function commit

---
+++ cloud/jobs.js
@@ -0,0 +1,28 @@
+//******Varibales Definition******//
+var classnameModule = require('./classname');
+var messageModule = require('./message');
+var messageModule = require('./message');
+var pushModule = require('./pushmessage');
+
+var AV = require('leanengine');
+
+AV.Cloud.job("CheckUpdateFlightJob", function(request, status) {
+  var Flight = AV.Object.extend(classnameModule.GetFlightClass());
+  var flightQuery = new AV.Query(Flight);
+	
+  flightQuery.lessThan("time", new Date());
+  flightQuery.notContainedIn("status",[messageModule.FlightStatus_Completed(), messageModule.FlightStatus_Full(), messageModule.FlightStatus_Overdue()]);
+
+  flightQuery.each(function(flight) {
+      // Set and save the change
+      flight.set("status", messageModule.FlightStatus_Overdue());
+      return flight.save();
+  }).then(function() {
+    // Set the job's success status
+    status.success("Flight Update completed successfully.");
+  }, function(error) {
+    // Set the job's error status
+	console.log(error.message);
+    status.error("Uh oh, something went wrong.");
+  });
+});
