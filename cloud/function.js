421947de1c866c9003d74fcca02dc64ba1ba870f
Byland Zhang <Byland Zhang>
2016-01-23 14:58:14 +0800

    update job function2

--- cloud/function.js
+++ cloud/function.js
@@ -562,3 +562,26 @@ AV.Cloud.define("GetShuikeRegistrationList", function (request, response) {
 				response.error(messageModule.errorMsg());
 			});
 })
+
+
+AV.Cloud.define("CheckUpdateFlightJob", function(request, status) {
+  var Flight = AV.Object.extend(classnameModule.GetFlightClass());
+  var flightQuery = new AV.Query(Flight);
+	
+  flightQuery.lessThan("time", new Date());
+  flightQuery.notContainedIn("status",[messageModule.FlightStatus_Completed(), messageModule.FlightStatus_Full(), messageModule.FlightStatus_Overdue()]);
+	
+  console.log("Update overdue Flights....");
+  flightQuery.each(function(flight) {
+      // Set and save the change
+	  console.log("Update overdue Flight: "+flight.id);
+      flight.set("status", messageModule.FlightStatus_Overdue());
+      return flight.save();
+  }).then(function() {
+    // Set the job's success status
+	console.log("Flight Update completed successfully");
+  }, function(error) {
+    // Set the job's error status
+	console.log(error.message);
+  });
+});
