886b5ed312ac894554a04a45373ee8aaa6a10597
BylandBo <zb19860119@gmail.com>
2016-02-03 23:32:02 +0800

    Update hook.js

--- cloud/hook.js
+++ cloud/hook.js
@@ -23,11 +23,6 @@ AV.Cloud.afterSave(classnameModule.GetShippingClass(), function(request) {
 	  var cargoOwner = cargo.get("owner");
 	  var cargoRelation = cargoOwner.relation('existShippings');
 	  cargoRelation.add(request.object);
-	  if(request.object.get("status") == messageModule.ShippingStatus_Received())
-	  {
-	   var oldScores = cargoOwner.get("scores");
-	   cargoOwner.set("scores",oldScores+10);
-	  }
 	  cargoOwner.save();
     },
     error: function(error) {
@@ -38,19 +33,14 @@ AV.Cloud.afterSave(classnameModule.GetShippingClass(), function(request) {
   flightQuery.include("owner");
   flightQuery.get(request.object.get('flight').id, {
     success: function(flight) {
-	  console.log("Hook function: update shipping to Flight: " + request.object.id)
+	  console.log("Hook function: update shipping to Flight: " + request.object.id);
       flight.addUnique("shippingList",request.object);
       flight.save();
 	  
-	  console.log("Hook function: update shipping to flightUser: " + request.object.id)
+	  console.log("Hook function: update shipping to flightUser: " + request.object.id);
 	  var flightOwner = flight.get("owner");
 	  var flightRelation = flightOwner.relation('existShippings');
 	  flightRelation.add(request.object);
-	  if(request.object.get("status") == messageModule.ShippingStatus_Received())
-	  {
-	   var oldScores2 = flightOwner.get("scores");
-	   flightOwner.set("scores",oldScores2+10);
-	  }
 	  flightOwner.save();
     },
     error: function(error) {
@@ -64,21 +54,57 @@ AV.Cloud.afterSave(classnameModule.GetPushMessageClass(), function(request) {
 		var userToQuery = new AV.Query(AV.User); 
 		console.log("Send to User Id: " + request.object.get("sendTo").id);
 		userToQuery.equalTo("objectId", request.object.get("sendTo").id);
-		if( request.object.get("type") != "system")
-		{
-			AV.Cloud.useMasterKey();
-				userToQuery.find().then(function (userTo) {
-					if(userTo.length <= 0)
-						console.log(messageModule.UserNotFound());
-					else
-					{
-						console.log("Hook function: update message "+request.object.id+" to User: " + userTo[0].id);
-						var messageToRelation = userTo[0].relation('existMessages');
-						messageToRelation.add(request.object);
-						userTo[0].save();
-					}
-				},function (error) {
-						console.log(error.message);
-				});
-	    }
+		AV.Cloud.useMasterKey();
+			userToQuery.find().then(function (userTo) {
+				if(userTo.length <= 0)
+					console.log(messageModule.UserNotFound());
+				else
+				{
+					console.log("Hook function: update message "+request.object.id+" to User: " + userTo[0].id);
+					var messageToRelation = userTo[0].relation('existMessages');
+					messageToRelation.add(request.object);
+					userTo[0].save();
+				}
+			},function (error) {
+					console.log(error.message);
+			});
+});
+
+AV.Cloud.afterUpdate(classnameModule.GetShippingClass(), function(request) {
+  var Cargo = AV.Object.extend(classnameModule.GetCargoClass());
+  var cargoQuery = new AV.Query(Cargo);
+  
+  var Flight = AV.Object.extend(classnameModule.GetFlightClass());
+  var flightQuery = new AV.Query(Flight);
+  
+   if(request.object.get("status") == messageModule.ShippingStatus_Received())
+   {
+	  cargoQuery.include("owner");
+	  cargoQuery.get(request.object.get('cargo').id, {
+		success: function(cargo) {	  
+		  console.log("Hook function: update CargoUser "+cargo.get("owner").id+" Score After completed shipping: " + request.object.id + ", Shipping status: "+ request.object.get("status"));
+		  var cargoOwner = cargo.get("owner");
+		  var oldScores = cargoOwner.get("scores");
+		  cargoOwner.set("scores",oldScores+10);
+		  cargoOwner.save();
+		},
+		error: function(error) {
+		   console.log('Got an error ' + error.code + ' : ' + error.message);
+		}
+	  });
+	  
+	  flightQuery.include("owner");
+	  flightQuery.get(request.object.get('flight').id, {
+		success: function(flight) {
+		  console.log("Hook function: update FlightUser "+flight.get("owner").id+" Score After completed shipping: " + request.object.id + ", Shipping status: "+ request.object.get("status"));
+		  var flightOwner = flight.get("owner");
+		  var oldScores2 = flightOwner.get("scores");
+		  flightOwner.set("scores",oldScores2+10);
+		  flightOwner.save();
+		},
+		error: function(error) {
+		   console.log('Got an error ' + error.code + ' : ' + error.message);
+		}
+	  });
+  }
 });
