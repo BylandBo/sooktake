806df312ccd8e79aa192e94e9f1c17b55b4f5a46
Byland Zhang <Byland Zhang>
2016-02-19 20:40:05 +0800

    update verion check and password reset6

--- cloud/function.js
+++ cloud/function.js
@@ -621,7 +621,7 @@ AV.Cloud.define("GetLatestAppVersion", function(request, response) {
 			var oldversion = Math.floor(currentVersion);
 			returnResults["latestVersion"] = config[0].get("value");
 			
-			console.log("Get latest version: current version->"+oldversion+ "; latestVersion->"+newversion + "; needUpdate->" + newversion != oldversion?'Yes':'No');
+			console.log("Get latest version: current version->"+oldversion+ "; latestVersion->"+newversion + "; needUpdate->" + (newversion != oldversion?'Yes':'No'));
 			if(newversion != oldversion)
 				returnResults["isMustUpdate"] = "YES";
 			else
