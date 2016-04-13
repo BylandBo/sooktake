778269bb906a4ef9556406d2630cd8e6ab246417
Byland Zhang <Byland Zhang>
2016-04-13 21:46:32 +0800

    commit latestversion check funtion

--- cloud/function.js
+++ cloud/function.js
@@ -613,7 +613,7 @@ AV.Cloud.define("GetLatestAppVersion", function(request, response) {
 	var platform = request.params.platform;
 	var Config = AV.Object.extend(classnameModule.GetConfigClass());
     var configQuery = new AV.Query(Config);
-	console.log("Get latest version: current version->"+currentVersion+ "; platform->"+platform+"; currentUser->"+AV.User.current().id);
+	console.log("Get latest version: current version->"+currentVersion+ "; platform->"+platform+"; currentUser->"+AV.User.current() == null?"":AV.User.current().id);
 	configQuery.equalTo("key", "latestAppVersion");
     configQuery.find().then(function (config) {
 	    if(config.length <= 0)
