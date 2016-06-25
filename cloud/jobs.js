45ba30c245c4c1e9c46a5fbe346c82d8e6fb613a
Byland Zhang <Byland Zhang>
2016-06-26 00:50:33 +0800

    update ping++

--- cloud/jobs.js
+++ cloud/jobs.js
@@ -3,7 +3,6 @@ var classnameModule = require('./classname');
 var messageModule = require('./message');
 var messageModule = require('./message');
 var pushModule = require('./pushmessage');
-var pingplusplus = require('./pingpluspluspay')
 
 var AV = require('leanengine');
 
