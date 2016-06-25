898e84b20a650f8af4b64c9e17c05d6584231d5c
Byland Zhang <Byland Zhang>
2016-06-26 03:36:04 +0800

    change ping++333555...

--- routes/pingpluspluswebhook.js
+++ routes/pingpluspluswebhook.js
@@ -12,7 +12,7 @@ router.post('/', function(request, response) {
   var postData = request.body;
   console.log(postData);
     try {
-      var event = JSON.parse(postData);
+      var event = postData;
       if (event.type === undefined) {
         return response('Event 对象中缺少 type 字段', 400);
       }
