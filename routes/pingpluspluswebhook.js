f2ab6e0109f66d9f0034ec855584804fb8e210d9
Byland Zhang <Byland Zhang>
2016-06-26 04:28:51 +0800

    change ping++

--- routes/pingpluspluswebhook.js
+++ routes/pingpluspluswebhook.js
@@ -11,29 +11,35 @@ router.post('/', function(request, response) {
   request.setEncoding('utf8');
   var postData = request.body;
   console.log(postData);
+  var resp = function (ret, status_code) {
+      response.writeHead(status_code, {
+        "Content-Type": "text/plain; charset=utf-8"
+      });
+      response.end(ret);
+    }
     try {
       var event = JSON.parse(JSON.stringify(postData));
 	  console.log(event);
       if (event.type === undefined) {
-        return response('Event 对象中缺少 type 字段', 400);
+        return resp('Event 对象中缺少 type 字段', 400);
       }
 	  console.log("event type: " + event.type);
       switch (event.type) {
         case "charge.succeeded":
           // 开发者在此处加入对支付异步通知的处理代码
-          return response("OK", 200);
+          return resp.writeHead("OK", 200);
           break;
         case "refund.succeeded":
           // 开发者在此处加入对退款异步通知的处理代码
-          return response("OK", 200);
+          return resp.writeHead("OK", 200);
           break;
         default:
-          return response("未知 Event 类型", 400);
+          return resp.writeHead("未知 Event 类型", 400);
           break;
       }
     } catch (err) {
 	  console.log(err);
-      return response('JSON 解析失败', 400);
+      return resp.writeHead('JSON 解析失败', 400);
     }
 });
 
