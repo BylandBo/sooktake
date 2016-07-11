cb80168b47733009c381c4056f6f23dfaed94393
Byland Zhang <Byland Zhang>
2016-07-11 23:09:29 +0800

    add payment history1

--- cloud/payment.js
+++ cloud/payment.js
@@ -40,7 +40,7 @@ AV.Cloud.define("PaymentTopup", function (request, response) {
 		{
 		    var sys = require('sys')
 			var exec = require('child_process').exec;
-			function puts(error, stdout, stderr) { console.log(stdout) };
+			function puts(error, stdout, stderr) { sys.puts(stdout) }
 			exec("ping api.pingxx.com", puts);
 			
 			var user = users[0];
