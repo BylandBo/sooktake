1193b0512630f7cf400150fc97b320ccb12a0df6
Byland Zhang <Byland Zhang>
2016-07-11 23:22:55 +0800

    add payment history2

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ var PORT = parseInt(process.env.LC_APP_PORT || 3000);
 app.listen(PORT, function () {
   console.log('Node app is running, port:', PORT);
 });
-app.timeout = 120000;
+app.timeout = 30000;
