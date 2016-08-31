5f889c556f51a3c1a62ac0ef6e4a0c17e6f87599
Byland Zhang <Byland Zhang>
2016-08-31 23:09:37 +0800

    first commit

--- cloud/pushmessage.js
+++ cloud/pushmessage.js
@@ -292,17 +292,10 @@ exports.PushShippingStatusUpdateToUser = function (shipping) {
 	   var paymentTypeInfo = "";
 	   if(shipping.get("cargo").get("payMethod") == messageModule.PF_PAYMENT_METHOD_SOONTAKE())
 		{
-		  if(cargo.get("expressType") == messageModule.expressPost())
-		  {
-		      paymentTypeInfo = "";
-		  }
-		  else
-		  {
 		    if(shipping.get("paymentStatus") != messageModule.PF_SHIPPING_PAYMENT_STATUS_SUCCESS())
 			  paymentTypeInfo = "(请及时付款)，";
 			else
 		      paymentTypeInfo = "(点'确认寄出包裹'后运费将从平台转入顺带君账户中，自此7天后系统将自动完成转账,请及时付款)，";
-		  }
 		}
 	   if(cargo.get("expressType") == messageModule.expressPost())
 		content = "亲，您的包裹["+shipping.get("cargo").get("type")+"]已寄出，"+paymentTypeInfo+"感谢您的支持！";
@@ -397,10 +390,19 @@ exports.PushShippingStatusUpdateToFlighter = function (shipping) {
 		content = "确认拿到包裹["+shipping.get("cargo").get("type")+"]"; 
 	if(status == messageModule.ShippingStatus_Received())
 	{
+	   var paymentTypeInfo = "";
+	   if(shipping.get("cargo").get("payMethod") == messageModule.PF_PAYMENT_METHOD_SOONTAKE())
+		{
+		    if(shipping.get("paymentStatus") != messageModule.PF_SHIPPING_PAYMENT_STATUS_SUCCESS())
+			  paymentTypeInfo = "请联系货主让Ta付款，";
+			else
+		      paymentTypeInfo = "货主确认寄出包裹后运费将从平台转入您账户中；如果货主没确认，7天后系统将自动完成转账,请联系货主让Ta付款)，";
+		}
+		
 	   if(cargo.get("expressType") == messageModule.expressPost())
-		content = "包裹["+shipping.get("cargo").get("type")+"]已寄出,(货主确认寄出包裹后运费将从平台转入您账户中；如果货主没确认，7天后系统将自动完成转账,请联系货主让Ta付款)，感谢您的支持！";
+		content = "包裹["+shipping.get("cargo").get("type")+"]已寄出，"+ paymentTypeInfo +"，感谢您的支持！";
 	   else
-		content = "包裹["+shipping.get("cargo").get("type")+"]已送达";
+		content = "包裹["+shipping.get("cargo").get("type")+"]已送达，"+ paymentTypeInfo +"，感谢您的支持！";
 	}
 	
 	//add message history
