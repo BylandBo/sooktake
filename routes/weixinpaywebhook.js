0b2798cfb49dc5d6f5125513275dcee8f73a9198
Byland Zhang <Byland Zhang>
2016-09-04 11:25:00 +0800

    add webhook

--- routes/weixinpaywebhook.js
+++ routes/weixinpaywebhook.js
@@ -1,17 +1,23 @@
 var router = require('express').Router();
+var util = require('/home/leanengine/app' + '/cloud/util');
 
 router.post('/', function(request, response, body) {
   request.setEncoding('utf8');
   console.log("Weixinpay Webhook received");
-  console.log("Weixinpay Webhook: " + JSON.stringify(request)); 
   
+  request.success = function(){ request.end(util.buildXML({ xml:{ return_code:'SUCCESS' } })); };
+  request.fail = function(){ request.end(util.buildXML({ xml:{ return_code:'FAIL' } })); };
+
+  util.pipe(request, function(err, data){
+		var xml = data.toString('utf8');
+		util.parseXML(xml, function(err, msg){
+			request.wxmessage = msg;	
+            console.log("Weixinpay Webhook received: " + msg);			
+		});
+  });
   AV.Cloud.run('WebCallFromWeiXin', { body: request }, {
-							success: function (paymentResult) {
-								
-							},
-							error: function (error) {
-								
-							}
+							success: function (paymentResult) {},
+							error: function (error) {}
   });
 						  
   var msg = "<xml><return_code><SUCCESS></return_code><return_msg><OK></return_msg></xml>"
