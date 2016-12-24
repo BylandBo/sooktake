5eecffa5195cbe3235c2b8da2822ce5a65f79e79
Byland Zhang <Byland Zhang>
2016-12-24 16:21:59 +0800

    update admin

--- cloud/function.js
+++ cloud/function.js
@@ -448,7 +448,10 @@ AV.Cloud.define("ApproveShuikeRegistration", function (request, response) {
 		var userDetail = userDetails[0];
 		var currentUser = userDetail.get("owner");
         // At this time myUser is filled with an Object containing type _pointer, objectid, etc.
-		userDetail.set("expiryDate",expiryDate);
+		if(expiryDate != '' && expiryDate != null)
+		{
+			userDetail.set("expiryDate",moment(expiryDate).format('DD-MM-YYYY'));
+		}
         if (isApprove == '1'){
             userDetail.set("status", messageModule.PF_USERDETAILS_STATUS_APPROVED());
 			currentUser.set("isVerify",messageModule.YES());
